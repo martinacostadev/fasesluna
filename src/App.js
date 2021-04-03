7709c3c03da38ee9f8727f9a997a78e3056b451f
kaizendevar <pmartinacosta@gmail.com>
2021-04-02 21:21:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function load_moon_phases(obj, callback) {
+  var gets = [];
+  for (var i in obj) {
+    gets.push(i + "=" + encodeURIComponent(obj[i]));
+  }
+  gets.push("LDZ=" + new Date(obj.year, obj.month - 1, 1) / 1000);
+  var xmlhttp = new XMLHttpRequest();
+  var url = "https://www.icalendar37.net/lunar/api/?" + gets.join("&");
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      callback(JSON.parse(xmlhttp.responseText));
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+function example_2(moon) {
+  var phMax = [];
+  for (var nDay in moon.phase) {
+    if (moon.phase[nDay].isPhaseLimit) {
+      phMax.push(
+        "<div>" +
+          "<span>" +
+          nDay +
+          "</span>" +
+          moon.phase[nDay].svg +
+          "<div>" +
+          moon.phase[nDay].phaseName +
+          "</div>" +
+          "</div>"
+      );
+    }
+  }
+  var width = 100 / phMax.length;
+  var html = "<b>" + moon.monthName + " " + moon.year + "</b>";
+  phMax.forEach(function (element) {
+    html += '<div style="width:' + width + '%">' + element + "</div>";
+  });
+  document.getElementById("moons").innerHTML = html;
+}
+var configMoon = {
+  lang: "en",
+  month: new Date().getMonth() + 1,
+  year: new Date().getFullYear(),
+  size: "100%",
+  lightColor: "rgb(255,255,230)",
+  shadeColor: "transparent",
+  texturize: true,
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  load_moon_phases(configMoon, example_2);
+
+  return <div id="moons"></div>;
 }
 
 export default App;
